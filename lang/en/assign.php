2b8b4a4368f021d9a4ec60b23d8c6872d0a13404
hari715om <singh.omhari715@gmail.com>
2025-08-06 13:56:09 +0530

    added new fields

--- lang/en/assign.php
+++ lang/en/assign.php
@@ -635,3 +635,7 @@ $string['nolatesubmissions'] = 'No late submissions accepted. ';
 $string['nosubmissionsacceptedafter'] = 'No submissions accepted after ';
 $string['notsubmittedyet'] = 'Not submitted yet';
 $string['submissionsnotgraded'] = 'Submissions not graded: {$a}';
+
+$string['batchcode'] = 'Batch Code';
+$string['centercode'] = 'Center Code';
+$string['studentid'] = 'Student ID';
